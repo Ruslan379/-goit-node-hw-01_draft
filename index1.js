875af4224a9b78f1443451b852687c7d099b57cf
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-14 14:50:33 +0200

    Дрога "40 node.js blgroup2" - 2

--- index1.js
+++ index1.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (req, res) => {
-    res.json({ message: "Hello friend!" });
+    res.json({ message: "Hello my friend!" });
 });
 
 app.listen(3000, () => console.log("Server is running on the port 3000!"));
