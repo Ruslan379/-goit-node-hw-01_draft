86816c20e456619d373fe9999b44f8a21afa76a5
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-11-18 16:06:53 +0200

    Node.js_02м_03з.Express-2==>54

--- routes/router.js
+++ routes/router.js
@@ -136,7 +136,8 @@ router.post("/users", async (req, res) => {
                 .required(),
 
             email: Joi.string()
-                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua', 'org',] } }),
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua', 'org',] } })
+                .required(),
 
             phone: Joi.string()
                 // .alphanum()
@@ -198,7 +199,8 @@ router.put("/users/:id", async (req, res) => {
                 .required(),
 
             email: Joi.string()
-                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua', 'org',] } }),
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua', 'org',] } })
+                .required(),
 
             phone: Joi.string()
                 // .alphanum()
@@ -272,9 +274,48 @@ router.put("/users/:id", async (req, res) => {
 router.patch("/users/:id", async (req, res) => {
     try {
         console.log("START-PATCH/:id".rainbow); //!
+        lineBreak();
+        //! ++++++++++++++ ВАЛИДАЦИЯ Joi +++++++++++++++++++++++++
+        const schema = Joi.object({
+            name: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'ua', 'org',] } })
+                .required(),
+
+            phone: Joi.string()
+                // .alphanum()
+                .min(5)
+                .max(12)
+                .required(),
+        });
+
+        const validationResult = schema.validate(req.body);
+        if (validationResult.error) {
+            console.log("Ошибка ВАЛИДАЦИИ:".bgRed.black);
+            console.log("");
+            console.log(validationResult.error);
+            lineBreak();
+            console.log("END-POST".yellow); //!
+            return res.status(400).json({ status: validationResult.error.details });
+        }
+        //! ___________________ ВАЛИДАЦИЯ Joi ___________________
+
         // const id = req.params.id; //1
         const { id } = req.params; //2
+
         const body = req.body; //! в index1.js ==> app.use(express.json());
+        const { name, email, phone } = body;
+        console.log("Эти поля прошли ВАЛИДАЦИЮ:".bgYellow.black);
+        console.log("");
+        console.log("name:".bgYellow.black, name.yellow); //!
+        console.log("email:".bgYellow.black, email.yellow); //!
+        console.log("phone:".bgYellow.black, phone.yellow); //!
+        lineBreak();
         console.log("Обновляем такие поля:".bgYellow.red, body);
 
         const users = await getUsersList();
